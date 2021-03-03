2cd8671138c04b800f188e988002514e4296c131
Loverboy <diadool@list.ru>
2021-03-03 13:10:32 +0500

    ready to accept 1.0 (configure = destroy)

--- app/src/main/java/com/example/firsttask/MainActivity.kt
+++ app/src/main/java/com/example/firsttask/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.firsttask
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
